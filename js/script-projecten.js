25eb4ad329f0c34ca47359fc4e65fd43380549bf
BenteCMD <112807135+BenteCMD@users.noreply.github.com>
2024-02-28 14:40:37 +0100

    Add files via upload

--- js/script-projecten.js
+++ js/script-projecten.js
@@ -16,7 +16,7 @@ var xxxploreProj = document.querySelector("#xxxplore")
 const enableDarkMode = () => {
     document.documentElement.classList.add("dark-mode")
     
-    menuProj.src="images/menu-dark.png"
+    menuProj.src="images/inclusive-dark.svg"
     fridgeProj.src="images/fridge-dark.png"
     skateProj.src="images/skate-dark.png"
     ccProj.src="images/cc-dark.png"
@@ -28,7 +28,7 @@ const enableDarkMode = () => {
 const disableDarkMode = () => {
     document.documentElement.classList.remove("dark-mode")
 
-    menuProj.src="images/menu-cloud.png"
+    menuProj.src="images/inclusive-cloud.svg"
     fridgeProj.src="images/fridge-cloud.png"
     skateProj.src="images/skate-cloud.png"
     ccProj.src="images/cc-cloud.png"
